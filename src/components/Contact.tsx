58f00025cb5962e1fd72ae2e36ab2f08e5af2b36
‘Lekky <lekkyjay@yahoo.com>
2025-02-22 18:40:05 +0100

    Adapted Contact component for darkMode

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -49,12 +49,12 @@ export default function Contact() {
 
       <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
         <div className="grid grid-cols-auto gap-6 mb-8">
-          <input type="text" placeholder="Enter your name" name="name" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white" />
-          <input type="email" placeholder="Enter your email" name="email" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white" />
+          <input type="text" placeholder="Enter your name" name="name" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" />
+          <input type="email" placeholder="Enter your email" name="email" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" />
         </div>
-        <textarea rows={6} placeholder="Enter your message" name="message" required className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"></textarea>
+        <textarea rows={6} placeholder="Enter your message" name="message" required className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90"></textarea>
 
-        <button type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500">
+        <button type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover">
           Submit <MdArrowForward />
         </button>
 
