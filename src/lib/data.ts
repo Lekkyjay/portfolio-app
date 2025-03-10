bc4b8b8ba35a22327475247b463d4e1a9af6f4dc
‘Lekky <lekkyjay@yahoo.com>
2025-03-10 17:47:02 +0100

    Added new project

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,53 +2,53 @@ import { RiFirebaseLine } from 'react-icons/ri'
 
 export const projectsData = [
   {
+    title: 'Courses Platform',
+    description: 'Online course enrollment platform using NextJS, Convex, UpStash, Resend, and Stripe',
+    url: 'https://courses-subscription.vercel.app',
+    bgImage: '/images/project-img-01.jpg',
+  },
+  {
     title: 'Real Estate App',
     description: 'React, TypeScript, React-Router, React-leaflet, React-Quill, Socket-IO, SASS',
     url: 'https://real-estate-client-lphp.onrender.com',
-    bgImage: '/images/project-img-01.jpg',
+    bgImage: '/images/project-img-02.jpg',
   },
   {
     title: 'E-Commerce App',
     description: 'React, TypeScript, Redux Toolkit, React-Router, Slick-Carousel',
     url: 'https://madingo.netlify.app/',
-    bgImage: '/images/project-img-02.jpg'
+    bgImage: '/images/project-img-03.jpg'
   },
   {
     title: 'E-commerce Clothing Store',
     description: 'React, TypeScript, Redux-Thunk, React-Router, React-Modal',
     url: 'https://dress-shop.netlify.app/',
-    bgImage: '/images/project-img-03.jpg'
+    bgImage: '/images/project-img-04.jpg'
   },
   {
     title: 'E-Commerce App',
     description: 'React, TypeScript, Redux Toolkit, React-Router, React-Slick, SASS',
     url: 'https://token-mall.netlify.app/',
-    bgImage: '/images/project-img-04.jpg'
+    bgImage: '/images/project-img-05.jpg'
   },
   {
     title: 'Healthy Grocery Shop App',
     description: 'Angular, TypeScript, Firebase, Angular Material, RxJs, Angular-Router',
     url: 'https://eshop-fcdf4.firebaseapp.com/',
-    bgImage: '/images/project-img-05.jpg',
+    bgImage: '/images/project-img-06.jpg',
   },
   {
     title: 'Admin KPI Dashboard',
     description: 'React, Redux, React-Router, Material-UI, Nivo-Chart,Express, Helmet, MongoDB',
     url: 'https://admin-dash-client.onrender.com',
-    bgImage: '/images/project-img-06.jpg'
-  },
-  {
-    title: 'Budgeting App with Angular Material',
-    description: 'Angular, TypeScript, Angular Material, RxJs, Angular-Router',
-    url: 'https://angular-budget-app2.netlify.app/',
     bgImage: '/images/project-img-07.jpg'
-  },
+  },  
   {
     title: 'Movie App',
     description: 'React, TypeScript, React-Router, TMDB-API',
-    url: 'https://movie-app-dev.netlify.app/',
+    url: 'https://movie-tmdb-api.netlify.app',    
     bgImage: '/images/project-img-08.jpg'
-  }
+  }  
 ]
 
 export const servicesData = [
